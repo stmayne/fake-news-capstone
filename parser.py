5613132077983cdbc57e177c4f4be7b6b2d4e479
Mills <dommil@8c8590470796.ant.amazon.com>
2018-07-11 18:16:12 -0700

    changed getBsType to return dictionary one-hot vector

--- parser.py
+++ parser.py
@@ -16,24 +16,48 @@ def parseUrl(url, suffix=False):
     return domain
 
 #function to pull data from bs detector dataset stored in S3
-#currently just getting the first type field
-#takes a domain name and checks the database for a match and returns type as string
+#takes a domain name and checks the database for a match and returns dict as one-hot vector
 def getBsType(domain):
     #pulling from s3
     s3 = boto3.resource('s3')
     object = s3.Object('fakenewstest','bs.json')
 
     data = json.load(object.get()['Body'])
-
-    #return types if found from the bs data
+    type_dict = {'bias':0, 'bias ':0, 'fake':0, 'conspiracy':0, 'unreliable':0, 'rumor':0, 'satire':0,'satirical':0, 'clickbait':0, 'political':0, 'credible':0, 'unknown':0, 'junkscience':0, 'junksci':0, 'hate':0}
+    #set type to 1 if found in bs data
     try:
         domain_data = data[domain]
-        type = [domain_data['type'], domain_data['2nd type'], domain_data['3rd type']]
+        types = [domain_data['type'], domain_data['2nd type'], domain_data['3rd type']]
+        for type in types:
+            if type in type_dict:
+                type_dict[type] = 1
     except:
-        type = ['','','']
+        print('failed to find domain in bs')
 
-    return type
+    #cleaning up data
+    if type_dict['bias '] == 1:
+        type_dict['bias'] = 1
+    if type_dict['junkscience'] == 1:
+        type_dict['junksci'] = 1
+    type_dict.pop('bias ')
+    type_dict.pop('junkscience')
 
+    return type_dict
+
+#find all types in bs dataset
+def findBSTypes():
+    s3 = boto3.resource('s3')
+    object = s3.Object('fakenewstest','bs.json')
+    data = json.load(object.get()['Body'])
+    type_dict = {}
+
+    for domain in data:
+        for type in data[domain]:
+            if type == 'Source Notes (things to know?)':
+                break
+            type_dict[data[domain][type]] = 1
+
+    print(type_dict)
 #function to get data from the webhose.io apitoken
 #currently takes domain name and returns domain rank, lots of other options available
 def getWebHoseData(domain):
@@ -63,4 +87,5 @@ if __name__ == "__main__":
     #bstype = getBsType(domain)
     #domain = parseUrl('https://www.nytimes.com/2018/07/02/nyregion/michael-cohen-trump.html?rref=collection%2Fsectioncollection%2Fpolitics&action=click&contentCollection=politics&region=stream&module=stream_unit&version=latest&contentPlacement=2&pgtype=sectionfront')
     #print(bstype)
-    print(getWebHoseData('cnn.com'))
+    print(getBsType('dcclothesline.com'))
+    #print(getWebHoseData('cnn.com'))
